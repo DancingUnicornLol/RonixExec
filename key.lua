b87b0ecbc0840773fdd8cdfb753e8863811f6c24
rscwn <96673832+rscwn@users.noreply.github.com>
2025-05-26 23:20:22 -0600

    Update key.lua

--- key.lua
+++ key.lua
@@ -9,6 +9,8 @@ local betaapi = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/libra
 betaapi.script_id = "7afc23713164c321d7fb3183d3af8bca";
 
 local is_beta = getgenv().isbeta or function() return false end
+local toasty = dtc.make_toast or function() end
+toasty("Ronix is Loading, Please wait..");
 
 local error_key_code = nil;
 local function iskeygucci(key)
