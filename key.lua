0d3d0911d617cab50ed3bd510029a962531114d7
rscwn <96673832+rscwn@users.noreply.github.com>
2025-08-18 16:49:17 -0600

    Update key.lua

--- key.lua
+++ key.lua
@@ -571,7 +571,7 @@ UI["10"].Activated:Connect(function()
 	local provider1 = Instance.new("TextButton")
 	provider1.Position = UDim2.new(0, 0, 0, 0)
 	provider1.Parent = buttonContainer
-	styleButton(provider1, "Lootlabs")
+	styleButton(provider1, "WorkInk")
 	
 	local provider2 = Instance.new("TextButton")
 	provider2.Position = UDim2.new(0, 0, 0.5, 5)
@@ -590,7 +590,8 @@ UI["10"].Activated:Connect(function()
     end)
 
     provider1.MouseButton1Click:Connect(function()
-        handle_choice("https://ads.luarmor.net/get_key?for=RonxiKey2-eEPAuyLEcNsd")
+        --// handle_choice("https://ads.luarmor.net/get_key?for=RonxiKey2-eEPAuyLEcNsd")
+		handle_choice("https://ads.luarmor.net/get_key?for=RonixAndroidWorkInk-UThuIlhplCjP");
     end)
 end);
 
@@ -620,3 +621,4 @@ end);
 UI["16"].Activated:Connect(function()
 	UI["1"]:Destroy();
 end);
+
