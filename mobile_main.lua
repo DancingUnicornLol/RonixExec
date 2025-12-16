6156c6cdd0ee79869ce4e4a1b80a274a78ad6b3e
DancingUnicornLol <Doggysecretfree@gmail.com>
2025-12-15 18:35:18 -0800

    Refactor HTTP request handling and cleanup code

--- mobile_main.lua
+++ mobile_main.lua
@@ -32,8 +32,7 @@ local http_get = function(url)
     local r = http_request({ Url = url });
     if r.Success then
         get_counter = 0;
- 
-		return r.Body;
+        return r.Body;
     end
 
     warn("HTTPGET FAIL REPORT THIS TO DEVS");
@@ -600,13 +599,11 @@ UI["13"].Activated:Connect(function()
     if (gucci) then
         updateStatus("Status: Access Granted!", Color3.fromRGB(100, 255, 100))
         task.wait(0.5)
-  		      save_key(key);
-					
-				
+       --// save_key(key);
         load_ui();
         
         if currentBlur then
-            Tweence:Create(currentBlur, TweenInfo.new(0.5), {Size = 0}):Play()
+            TweenService:Create(currentBlur, TweenInfo.new(0.5), {Size = 0}):Play()
             task.delay(0.5, function() currentBlur:Destroy() end)
         end
         UI["1"]:Destroy();
@@ -631,7 +628,3 @@ UI["16"].Activated:Connect(function()
         task.delay(0.5, function() currentBlur:Destroy() end)
     end
 end)
-	
-
-
-
